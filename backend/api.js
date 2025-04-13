369b7b0908b50b8a84ddf28dfa7d4c7ebf33f607
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2025-04-12 22:00:56 -0300

    testanto o sistema de login

--- backend/api.js
+++ backend/api.js
@@ -1,17 +1,34 @@
 const express = require('express')
+const jwt = require('json-web-token')
 
 const app = express()
 const PORT = 1995
+const JWTSECRET = process.env.SECRETJWT
 
-app.use(express.json)
+app.get('/', (req, res) => {
+    res.json({
+        title: 'The Chikitaverse Inc.',
+        url: 'http://chikitaverse/',
+        code: 'None'
+    })
+})
 
-app.get('/', (res, req) => {
-    res.json([{
-        test: 'Daniel',
-        id: 1
-    }])
+app.get('/client', (req, res) => {
+    res.json({
+        message: 'Not Found',
+        code: 404
+    })
+})
+
+app.post('/login' , (req, res) => {
+    if (req.body.user === 'Daniel' && req.body.password === '12345') {
+        jwt.sign({ id: 1 }, JWTSECRET)
+        return res.end()
+    }
+    
+    res.status(401).end()
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor esta em http://localhost:${PORT}`)
+    console.log(`Api esta em http://localhost:${PORT}/`)
 })
