1e37d1b61a0bd9e8540177d16e6f09de7c9b1bef
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2025-05-09 17:59:41 -0300

    Testando coisas

--- frontend/src/components/ui/MenuBar/MenuBar.jsx
+++ frontend/src/components/ui/MenuBar/MenuBar.jsx
@@ -1,56 +1,56 @@
 "use client"
 import styles from './MenuBar.module.css'
 import '../../css/theme.css'
+import { useEffect } from 'react'
 
 //! Components
 import MenuOption from '../MenuOption/MenuOption'
 
-class MobileNavbar {//Todo: Fazer o menu funcionar
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".menu",
-    ".iten",
-    ".iten li",
-  );
-  mobileNavbar.init();
-
 export default function NavBar() {
+   
+  useEffect(() => {
+    class MobileNavbarClass {
+      constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = 'active';
+
+        this.handleClick = this.handleClick.bind(this);
+      }
+
+      animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = '')
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+              }s`);
+        });
+      }
+
+      handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+      }
+
+      addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+      }
+
+      init() {
+        if (this.mobileMenu) {
+          this.addClickEvent();
+        }
+        return this;
+      }
+    }
+
+    const mobileNavbar = new MobileNavbarClass('.menu', '.iten', '.iten li');
+    mobileNavbar.init();
+  }, []);
+    
     return (
         <nav className={styles.menu}>
             <div className={styles.menubar1}></div>
