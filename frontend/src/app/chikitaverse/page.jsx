04d193e049ea9423c505cf5060150239eb6b1c4f
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-06-15 11:38:46 -0300

    ajuste de umas coisas

--- frontend/src/app/chikitaverse/page.jsx
+++ frontend/src/app/chikitaverse/page.jsx
@@ -19,7 +19,7 @@ export default function Chikitaverse() {
                     <span>
                         <CardChikitaverse logo='chess.png' link='https://www.chess.com/club/the-chikitaverse-inc/join' title='Clube de Xadrez' game='Chess.com' />
                             <CardChikitaverse logo='roblox.png' link='https://www.roblox.com/pt/communities/35567222/The-Chikitaverse-Inc#!/about' title='TCInc. Comunidade' game='Roblox'/>
-                                <CardChikitaverse logo='roblox.png' link='https://ro.blox.com/Ebh5?af_dp=roblox%3A%2F%2Fnavigation%2Fgame_details%3FgameId%3D7250922134&af_web_dp=https%3A%2F%2Fwww.roblox.com%2Fgames%2F108630188152409' title='Chikita Voice' game='Roblox'/>
+                                <CardChikitaverse logo='roblox.png' link='https://www.roblox.com/pt/games/91016125083941/ChikitaVoice-Acesso-antecipado' title='Chikita Voice' game='Roblox'/>
                     </span>
                     <section>
                         <h2>Atividade:</h2>
